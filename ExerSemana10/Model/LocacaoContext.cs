695458c784dc9834fa624118ab6fcb7255437867
alessandramedeirosvieira <alessandra_m_vieira@estudante.sesisenai.org.br>
2023-04-09 00:20:31 -0300

    first add

---
+++ ExerSemana10/Model/LocacaoContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerSemana10.Model
+{
+    public class LocacaoContext  : DbContext
+    {
+      public LocacaoContext(DbContextOptions<LocacaoContext> options) : base(options)
+      {
+
+      }
+      public DbSet<CarroModel> Carro {get; set;}
+      public DbSet<MarcaModel> Marca {get; set;}
+      
+    }
+}
